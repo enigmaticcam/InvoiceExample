16a1e59320e61c0c4b1af19caa6644300a48c1d8
Tangen, Cameron <Cameron.Tangen@tweglobal.com>
2026-07-31 10:19:34 -0700

    Created WPF Project with InvoiceSearchView and InvoiceHeaderModel.

---
+++ Invoice_WPF/Models/InvoiceHeaderModel.cs
@@ -0,0 +1,10 @@
+﻿namespace Invoice_WPF.Models;
+
+public class InvoiceHeaderModel
+{
+    public int InvoiceHeaderId { get; set; }
+    public int Customer { get; set; }
+    public DateOnly InvoiceDate { get; set; }
+    public int StatusTypeId { get; set; }
+    public string Description { get; set; }
+}
